17ea4495255e0cd8337e8d5b70b2f058c4712d95
Séverin Lemaignan <severin@guakamole.org>
2016-05-20 23:13:26 +0100

    Bumped to 1.0

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from distutils.core import setup
 
 setup(name='consoletk',
-      version='0.1',
-      description='ANSI-based console toolkit',
+      version='1.0',
+      description='stand-alone, ANSI-based console UI toolkit',
       author='Séverin Lemaignan',
       author_email='severin@guakamole.org',
       url='https://github.com/severin-lemaignan/consoletk',
